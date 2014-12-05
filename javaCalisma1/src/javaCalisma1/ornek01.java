c7c57f0f936bcec4630b1a204a676455ad5003c0
boyraz <boyraz@10.50.13.144>
2014-12-05 14:54:55 +0200

    first example

---
+++ javaCalisma1/src/javaCalisma1/ornek01.java
@@ -0,0 +1,12 @@
+package javaCalisma1;
+
+public class ornek01 {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");		
+		System.out.print("BTBS2");
+		System.out.println("KAVA");
+		System.out.printf("sayi: %d",5);
+		System.out.printf("at: %s","kadir");
+	}
+}
